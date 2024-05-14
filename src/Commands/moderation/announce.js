9831a0efb173ce26e716f4f1ce5d4fccaf94a30c
jed-cell <56687470+jed-cell@users.noreply.github.com>
2024-05-13 22:18:12 -0700

    Update announce.js

--- src/Commands/moderation/announce.js
+++ src/Commands/moderation/announce.js
@@ -1,47 +1,54 @@
-const { ApplicationCommandOptionType, PermissionsBitField, EmbedBuilder, ChannelType, PermissionFlagsBits,} = require('discord.js');
+const { ApplicationCommandOptionType, Permissions } = require('discord.js');
+
 module.exports = {
     name: 'announce',
-    description: 'To announce in any channel',
-   deleted:true,
+    deleted:"true",
+    description: 'Announce in any channel',
     options: [
         {
-            name:'title',
-            description:' The Title of the embed',
-            required: true,
+            name: 'title',
+            description: 'The title of the announcement', // Adjusted description
             type: ApplicationCommandOptionType.String,
-        },
-        {
-          name: 'text',
-          description: 'Description of the text you want to send.',
-          required: true,
-          type: ApplicationCommandOptionType.String,
-        },
-        {
-            name:'channel',
-            description:'The channel you want to send the message in',
             required: true,
-            type: ApplicationCommandOptionType.Channel,
         },
-  
-    ],  
-    permissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
+        {
+            name: 'text',
+            description: 'Description of the announcement', // Adjusted description
+            type: ApplicationCommandOptionType.STRING,
+            required: true,
+        },
+        {
+            name: 'channel',
+            description: 'The channel where you want to send the message',
+            type: ApplicationCommandOptionType.CHANNEL,
+            required: true,
+        },
+    ],
+    permissionsRequired: ['ADMINISTRATOR'],
+    botPermissions: ['ADMINISTRATOR'],
     callback: async (client, interaction) => {
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.reply({ content: `You don't have the permissions to create an announcement!`, ephemeral: true});
+        if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+            return interaction.reply({ content: `You don't have permission to create an announcement!`, ephemeral: true });
+        }
         
         const channel = interaction.options.getChannel('channel');
         const title = interaction.options.getString('title');
         const description = interaction.options.getString('text');
+        if (description.length > 100) {
+            description = description.substring(0, 100); // Truncate to 100 characters
+        }
+        const embed = {
+            color: 'BLUE',
+            title: title,
+            description: description,
+        };
 
-        const embed = new EmbedBuilder()
-        .setColor("Blue")
-        .setTitle(`${title}`)
-        .setDescription(`${description}`)
-
-        await channel.send({content:`||@everyone||`, embeds: [embed]}).catch(err => {
-            return interaction.reply(`I had a problem posting the announcement in ${channel}!`);
-        })
-        await interaction.reply({ content: `👌 I successfully posted the announcement in #${channel}!`, ephemeral: true});
+        try {
+            await channel.send({ content: `@everyone`, embeds: [embed] });
+            await interaction.reply({ content: `👌 Successfully posted the announcement in #${channel}!`, ephemeral: true });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply(`There was an issue posting the announcement in ${channel}!`);
+        }
     },
-
 };
