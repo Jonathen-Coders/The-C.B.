5a4bd8f299210640f352426b960b4c229fe87f61
jed-cell <56687470+jed-cell@users.noreply.github.com>
2024-05-14 01:45:30 -0700

    chore: Update announce.js
    Update announce.js file to use the `EmbedBuilder` class from the `discord.js` library instead of `MessageEmbed`. This change improves the code by adopting the latest API and ensures compatibility with future updates.
    
    Note: Remove the WIP comment.

--- src/Commands/moderation/announce.js
+++ src/Commands/moderation/announce.js
@@ -1,8 +1,8 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits, PermissionsBitField, Permissions } = require('discord.js');
-const {MessageEmbed} = require('discord.js');
+const { ApplicationCommandOptionType, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'announce',
-    deleted:true,
+    deleted:false,
     description: 'Announce in any channel',
     options: [
         {
@@ -14,50 +14,45 @@ module.exports = {
         {
             name: 'text',
             description: 'Description of the announcement',
-            type: ApplicationCommandOptionType.String, // Add missing type field
+            type: ApplicationCommandOptionType.String,
             required: true,
         },
         {
             name: 'channel',
             description: 'The channel where you want to send the message',
-            type: ApplicationCommandOptionType.Channel, // Add missing type field
+            type: ApplicationCommandOptionType.Channel,
             required: true,
         },
         {
             name: 'ping',
             description: 'Ping everyone',
-            type: ApplicationCommandOptionType.Boolean, // Add missing type field
+            type: ApplicationCommandOptionType.Boolean,
             required: true,
         },
-
-    ],
-    permissionsRequired: ['ADMINISTRATOR'],
-    botPermissions: ['ADMINISTRATOR'],
-    callback: async (client, interaction) => {
-        if (!interaction.member.permissions.has(PermissionFlagsBits.ADMINISTRATOR)) {
-            return interaction.reply({ content: `You don't have permission to create an announcement!`, ephemeral: true });
-        }
-        const mention = interaction.options.getMentionable('mention');
-        const channel = interaction.options.getChannel('channel');
-        if (!channel.permissionsFor(client.user).has(PermissionFlagsBits.ADMINISTRATOR)) {
-            return interaction.reply({ content: `I don't have permission to post in this channel!`, ephemeral: true });
-        }
-        const title = interaction.options.getString('title');
-const description = interaction.options.getString('text');
-if (description.length > 2048) {
-    return interaction.reply({ content: `The description is too long! It must be 2048 characters or less.`, ephemeral: true });
-}
-const ping = interaction.options.getBoolean('ping');
-const embed = new MessageEmbed()
-.setColor('BLUE')
-.setTitle(title)
-.setDescription(description);
+        ],
+        permissionsRequired: ['ADMINISTRATOR'],
+        botPermissions: ['ADMINISTRATOR'],
+        callback: async (client, interaction) => {
+            if (!interaction.member.permissions.has(PermissionFlagsBits.ADMINISTRATOR)) {
+                return interaction.reply({ content: `You don't have permission to create an announcement!`, ephemeral: true });
+            }
+            const ping = interaction.options.getBoolean('ping');
+            const channel = interaction.options.getChannel('channel');
+            if (!channel.permissionsFor(client.user).has(PermissionFlagsBits.ADMINISTRATOR)) {
+                return interaction.reply({ content: `I don't have permission to post in this channel!`, ephemeral: true });
+            }
+            const title = interaction.options.getString('title');
+            const description = interaction.options.getString('text');
+            if (description.length > 2048) {
+                return interaction.reply({ content: `The description is too long! It must be 2048 characters or less.`, ephemeral: true });
+            }
+            const embed = new EmbedBuilder()
+            .setColor('#ff0000')
+            .setTitle(title)
+            .setDescription(description);
             let content = '';
             if (ping) {
-                content += '@everyone ';
-            }
-            if (mention) {
-                content += `${mention} `;
+                content += '<@&1226729554681270375> ';
             }
         
             try {
@@ -68,4 +63,4 @@ const embed = new MessageEmbed()
                 await interaction.reply(`There was an issue posting the announcement in ${channel.name}!`);
             }
         },
-    }
+        }
---
+++ src/Commands/moderation/purge.js
@@ -0,0 +1,78 @@
+
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
+// Import the necessary Discord.js modules
+
+module.exports = {
+    name: 'purge',
+    description: 'Delete a specified number of messages',
+    options: [
+        {
+            name: 'amount',
+            description: 'The number of messages to delete',
+            type: ApplicationCommandOptionType.Integer,
+            required: true,
+        },
+        {
+            name: 'target',
+            description: 'The target of the purge (user, bots, or all)',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+            choices: [
+                {
+                    name: 'User',
+                    value: 'user',
+                },
+                {
+                    name: 'Bots',
+                    value: 'bots',
+                },
+                {
+                    name: 'All',
+                    value: 'all',
+                },
+            ],
+        },
+    ],
+    callback: async (client, interaction) => {
+        // Check if the user has the required permissions
+        if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+            return interaction.reply('You do not have permission to use this command.');
+        }
+
+        // Get the amount and target from the interaction options
+        const amount = interaction.options.getInteger('amount');
+        const target = interaction.options.getString('target');
+
+        // Check if the amount is a valid number
+        if (isNaN(amount) || amount <= 0 || amount > 100) {
+            return interaction.reply('Please provide a valid number between 1 and 100 for the amount.');
+        }
+
+        // Fetch the specified number of messages
+        let fetched;
+        let fetchAmount = Math.min(amount, 99) + 1; // Ensure the fetch amount is within the limit
+        if (target === 'user') {
+            fetched = await interaction.channel.messages.fetch({ limit: fetchAmount, before: interaction.id, user: interaction.user.id });
+        } else if (target === 'bots') {
+            fetched = await interaction.channel.messages.fetch({ limit: fetchAmount, before: interaction.id, user: interaction.client.user.id });
+        } else {
+            fetched = await interaction.channel.messages.fetch({ limit: fetchAmount, before: interaction.id });
+        }
+        let messagesToDelete = fetched.filter(msg => Date.now() - msg.createdTimestamp < 14 * 24 * 60 * 60 * 1000);
+
+// Delete the fetched messages
+interaction.channel.bulkDelete(messagesToDelete)
+    .then(() => {
+        // Send a success message
+        interaction.reply(`Successfully deleted ${messagesToDelete.size} messages.`)
+            .then(msg => {
+                // Automatically delete the success message after 5 seconds
+                msg.delete({ timeout: 5000 });
+            });
+    })
+    .catch(err => {
+        console.error(err);
+                interaction.reply(`An error occurred while deleting messages.${err}`);
+            });
+    },
+};
