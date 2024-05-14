1f4008c77665eaece21bc470e41a957ef30bdd57
jed-cell <56687470+jed-cell@users.noreply.github.com>
2024-05-13 23:57:33 -0700

    chore: Update dependencies and limit announcement description to 100 characters WIP: the command keeps freeking out so I need to rewite the entire code and go from there

--- src/Commands/moderation/announce.js
+++ src/Commands/moderation/announce.js
@@ -1,54 +1,67 @@
-const { ApplicationCommandOptionType, Permissions } = require('discord.js');
-
+const { ApplicationCommandOptionType, PermissionFlagsBits, PermissionsBitField, Permissions } = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 module.exports = {
     name: 'announce',
-    deleted:"true",
+    deleted:true,
     description: 'Announce in any channel',
     options: [
         {
             name: 'title',
-            description: 'The title of the announcement', // Adjusted description
+            description: 'The title of the announcement',
             type: ApplicationCommandOptionType.String,
             required: true,
         },
         {
             name: 'text',
-            description: 'Description of the announcement', // Adjusted description
-            type: ApplicationCommandOptionType.STRING,
+            description: 'Description of the announcement',
+            type: ApplicationCommandOptionType.String, // Add missing type field
             required: true,
         },
         {
             name: 'channel',
             description: 'The channel where you want to send the message',
-            type: ApplicationCommandOptionType.CHANNEL,
+            type: ApplicationCommandOptionType.Channel, // Add missing type field
             required: true,
         },
+        {
+            name: 'ping',
+            description: 'Ping everyone',
+            type: ApplicationCommandOptionType.Boolean, // Add missing type field
+            required: true,
+        },
+
     ],
+
     permissionsRequired: ['ADMINISTRATOR'],
     botPermissions: ['ADMINISTRATOR'],
     callback: async (client, interaction) => {
-        if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+        if (!interaction.member.permissions.has(PermissionFlagsBits.ADMINISTRATOR)) {
             return interaction.reply({ content: `You don't have permission to create an announcement!`, ephemeral: true });
         }
         
         const channel = interaction.options.getChannel('channel');
+        if (!channel.permissionsFor(client.user).has(PermissionFlagsBits.ADMINISTRATOR)) {
+            return interaction.reply({ content: `I don't have permission to post in this channel!`, ephemeral: true });
+        }
         const title = interaction.options.getString('title');
         const description = interaction.options.getString('text');
-        if (description.length > 100) {
-            description = description.substring(0, 100); // Truncate to 100 characters
-        } else( interaction.reply('sorry please fix this your discription must be less than 100'));
-        const embed = {
-            color: 'BLUE',
-            title: title,
-            description: description,
-        };
-
-        try {
-            await channel.send({ content: `@everyone`, embeds: [embed] });
-            await interaction.reply({ content: `👌 Successfully posted the announcement in #${channel}!`, ephemeral: true });
-        } catch (error) {
-            console.error(error);
-            await interaction.reply(`There was an issue posting the announcement in ${channel}!`);
-        }
-    },
-};
+        const ping = interaction.options.getBoolean('ping');
+        const embed = new MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(title)
+        .setDescription(description);
+            let content = '';
+            if (ping) {
+                content += '@everyone ';
+            }
+            
+        
+            try {
+                await channel.send({ content: content, embeds: [embed] });
+                await interaction.reply({ content: `👌 Successfully posted the announcement in #${channel.name}!`, ephemeral: true });
+            } catch (error) {
+                console.error(error);
+                await interaction.reply(`There was an issue posting the announcement in ${channel.name}!`);
+            }
+        },
+    }
