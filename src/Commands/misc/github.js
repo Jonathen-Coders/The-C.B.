d748b7cf78ccc43782942497e7db46f9e1ee904e
jed-cell <56687470+jed-cell@users.noreply.github.com>
2024-05-13 21:09:04 -0700

    Create github.js
    Github command

---
+++ src/Commands/misc/github.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'github',
+    description: 'The Github repository',
+    deleted: false,
+    callback: async (client, interaction) => {
+    interaction.reply(`You want to view changes and commits to github? here ya go. https://github.com/Jonathen-Coders/The-C.B.`)
+    },
+  };
