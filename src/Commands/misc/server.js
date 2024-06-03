137d09b4a0cfad26783a8666ca70e3234ef5265b
jed-cell <56687470+jed-cell@users.noreply.github.com>
2024-06-03 01:42:47 -0700

    chore: Update server-invite command to generate invite link for the bot
    Refactor the server-invite command to generate an invite link for the bot to join the user's server. This improves the functionality of the command by providing a convenient way for users to invite the bot. Additionally, handle errors that may occur during the invite link generation process.
    
    Fixes #N/A

--- src/Commands/misc/server.js
+++ src/Commands/misc/server.js
@@ -1,8 +1,24 @@
+const { ApplicationCommandOptionType } = require('discord.js');
+
 module.exports = {
-    name: 'sinvite',
-    description: 'Come to our server',
+    name: 'server-invite',
     deleted: false,
+    description: 'Generate an invite link for the bot to join your server.',
     callback: async (client, interaction) => {
-    interaction.reply(`You are invited to my main server. https://discord.gg/a6m3cfEdkn `)
+        try {
+            // Get the current channel where the command was executed
+            const channel = interaction.channel;
+
+            // Create an invite link for the bot to join the server
+            const invite = await channel.createInvite({ unique: true, temporary: false });
+
+            // Construct the full invite link
+            const inviteLink = `https://discord.gg/${invite.code}`;
+
+            interaction.reply(`Invite the bot to your server by clicking this link: ${inviteLink}`);
+        } catch (error) {
+            console.error(error);
+            interaction.reply('An error occurred while generating the server invite link.');
+        }
     },
   };
