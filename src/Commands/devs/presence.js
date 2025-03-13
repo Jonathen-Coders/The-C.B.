5ec3f4eb808f4d9a5a2ba50156125ebdc51b1a9e
jed-cell <56687470+jed-cell@users.noreply.github.com>
2025-03-13 20:23:40 +0000

    New change

--- src/Commands/devs/presence.js
+++ src/Commands/devs/presence.js
@@ -1,76 +1,75 @@
-
-const { ApplicationCommandOptionType, ActivityType } = require('discord.js');
+const { ApplicationCommandOptionType, ActivityType } = require("discord.js");
 
 module.exports = {
     devOnly: true,
-    name: 'presence',
-    description: 'Sets the bot\'s presence.',
+    name: "presence",
+    description: "Sets the bot's presence.",
     options: [
         {
-            name: 'status',
-            description: 'The status',
+            name: "status",
+            description: "The status",
             type: ApplicationCommandOptionType.String,
             required: true,
             choices: [
                 {
-                    name: 'Online',
-                    value: 'online',
+                    name: "Online",
+                    value: "online",
                 },
                 {
-                    name: 'Idle',
-                    value: 'idle',
+                    name: "Idle",
+                    value: "idle",
                 },
                 {
-                    name: 'Do Not Disturb',
-                    value: 'dnd',
+                    name: "Do Not Disturb",
+                    value: "dnd",
                 },
                 {
-                    name: 'Invisible',
-                    value: 'invisible',
-                }
+                    name: "Invisible",
+                    value: "invisible",
+                },
             ],
         },
         {
-            name: 'activity',
-            description: 'The activity type',
+            name: "activity",
+            description: "The activity type",
             type: ApplicationCommandOptionType.String,
-            required: true,
+            required: false,
             choices: [
                 {
-                    name: 'Playing',
-                    value: 'Playing',
+                    name: "Playing",
+                    value: "Playing",
                 },
                 {
-                    name: 'Watching',
-                    value: 'Watching',
+                    name: "Watching",
+                    value: "Watching",
                 },
                 {
-                    name: 'Listening',
-                    value: 'Listening',
+                    name: "Listening",
+                    value: "Listening",
                 },
                 {
-                    name: 'Streaming',
-                    value: 'Streaming',
+                    name: "Streaming",
+                    value: "Streaming",
                 },
                 {
-                    name: 'Competing',
-                    value: 'Competing',
-                }
+                    name: "Competing",
+                    value: "Competing",
+                },
             ],
         },
         {
-            name: 'message',
-            description: 'The status message',
+            name: "message",
+            description: "The status message",
             type: ApplicationCommandOptionType.String,
-            required: true,
+            required: false,
         },
     ],
     callback: async (client, interaction) => {
         try {
             // Get the status, activity type, and status message from the command options
-            const status = interaction.options.getString('status');
-            const activityType = interaction.options.getString('activity');
-            const statusMessage = interaction.options.getString('message');
+            const status = interaction.options.getString("status");
+            const activityType = interaction.options.getString("activity");
+            const statusMessage = interaction.options.getString("message");
 
             // Set the bot's presence
             await client.user.setPresence({
@@ -80,20 +79,23 @@ module.exports = {
                         type: ActivityType[activityType],
                     },
                 ],
-                status: status,  // online, idle, dnd, or invisible
+                status: status, // online, idle, dnd, or invisible
             });
 
             await interaction.reply({
-                content: `✅ Bot's presence set to: ${status} - ${activityType} ${statusMessage}`, 
-                ephemeral: true 
+                content: `✅ Bot's presence set to: ${status} - ${activityType} ${statusMessage}`,
+                ephemeral: true,
             });
-            console.log(`Bot's presence set to: ${status} - ${activityType} ${statusMessage}`);
-            
+            console.log(
+                `Bot's presence set to: ${status} - ${activityType} ${statusMessage}`,
+            );
         } catch (error) {
             console.error(error);
             await interaction.reply({
-                content: 'An error occurred while setting the presence: ' + error.message,
-                ephemeral: true
+                content:
+                    "An error occurred while setting the presence: " +
+                    error.message,
+                ephemeral: true,
             });
         }
     },
